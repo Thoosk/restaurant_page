f2999c0935484ad29546f10b0b5c356ee08ec8a9
Thoosk <thomas.skant@gmx.at>
2020-12-10 17:36:44 +0100

    set up main page and styling

---
+++ src/initial_page.js
@@ -0,0 +1,9 @@
+function loadHeader() {
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+}
+
+function loadMain() {
+    const 
+}
